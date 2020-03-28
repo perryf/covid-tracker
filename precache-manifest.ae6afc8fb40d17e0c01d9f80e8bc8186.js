3562ef53baf6614dd8917a1223261905b282495b
Perry Fustero <pjfust@gmail.com>
2020-03-28 17:40:37 -0400

    Updates

---
+++ precache-manifest.ae6afc8fb40d17e0c01d9f80e8bc8186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56863f6c0f46671e57480fe5a2cf1ee4",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "908e6dfaa5479f510a41",
+    "url": "/covid-tracker/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "854741c1e6278b694d7f",
+    "url": "/covid-tracker/static/js/2.2fa823e9.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/covid-tracker/static/js/2.2fa823e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "908e6dfaa5479f510a41",
+    "url": "/covid-tracker/static/js/main.d713e936.chunk.js"
+  },
+  {
+    "revision": "cec2b88799a2eff32207",
+    "url": "/covid-tracker/static/js/runtime-main.53994d34.js"
+  }
+]);
